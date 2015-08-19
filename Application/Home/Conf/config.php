43c513fe1ad113f29577367bfe9b3cc4a16730f7
sailoryoung <sailor0young@gmail.com>
2015-08-19 17:35:12 +0800

    新建目录结构，初步替换模板内容

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -1,4 +1,8 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+	
+	//模板布局
+	'LAYOUT_ON' => false,
+	'LAYOUT_NAME' => 'layout'
 );
---
+++ Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -0,0 +1,220 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+	<meta charset="UTF-8">
+	<title>思迪亚体育</title>
+	<link rel="stylesheet" type="text/css" href="/sdysports/Public/css/style.css">
+	<link rel="stylesheet" type="text/css" href="/sdysports/Public/css/share.css">
+	<link rel="stylesheet" type="text/css" href="/sdysports/Public/css/jquery.fullScreenSlider.css">
+</head>
+<body>
+	<header>
+		<nav>
+			<img src="/sdysports/Public/images/logo.gif">
+			<ul>
+				<li><a href="#">首页</a></li>
+				<li><a href="#">思迪亚体育联盟</a></li>
+				<li><a href="course.html">思迪亚课程计划</a></li>
+				<li><a href="#">思迪亚梦工厂</a></li>
+				<li><a href="#">思迪亚在中国</a></li>
+				<li><a href="#">关于思迪亚</a></li>
+				<div style="clear:both;"></div>			
+			</ul>
+		</nav>
+		<div id="slider">
+			<ul class="slider">
+				<li>
+					<img src="/sdysports/Public/images/banner_1.jpg" alt="图片展示">
+					<div>
+						<h1>标题文字1</h1>
+						<p>这是描述这一张图片的文字，后台上传时添加</p>
+					</div>
+				</li>
+				<li>
+					<img src="/sdysports/Public/images/banner_1.jpg" alt="图片展示">
+					<div>
+						<h1>标题文字2</h1>
+						<p>这是描述这一张图片的文字，后台上传时添加</p>
+					</div>
+				</li>
+				<li>
+					<img src="/sdysports/Public/images/banner_1.jpg" alt="图片展示">
+					<div>
+						<h1>标题文字3</h1>
+						<p>这是描述这一张图片的文字，后台上传时添加</p>
+					</div>
+				</li>
+				<li>
+					<img src="/sdysports/Public/images/banner_1.jpg" alt="图片展示">
+					<div>
+						<h1>标题文字4</h1>
+						<p>这是描述这一张图片的文字，后台上传时添加</p>
+					</div>
+				</li>
+				<li>
+					<img src="/sdysports/Public/images/banner_1.jpg" alt="图片展示">
+					<div>
+						<h1>标题文字5</h1>
+						<p>这是描述这一张图片的文字，后台上传时添加</p>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</header>
+	<div id="section2">
+		<ul id="describe">
+			<li>
+				<img src="/sdysports/Public/images/set2.gif">
+				<div>
+					<p>
+						<span class="title">快乐篮球</span>
+						<a href="#">即刻启程</a>
+						<span class="rank rank5"></span>
+					</p>
+				</div>
+			</li>
+			<li>
+				<img src="/sdysports/Public/images/set1.gif">
+				<div>
+					<p>
+						<span class="title">激情篮球</span>
+						<a href="#">即刻启程</a>
+						<span class="rank rank4"></span>
+					</p>
+				</div>
+			</li>
+			<li>
+				<img src="/sdysports/Public/images/set2.gif">
+				<div>
+					<p>
+						<span class="title">中考篮球</span>
+						<a href="#">即刻启程</a>
+						<span class="rank rank2"></span>
+					</p>
+				</div>
+			</li>
+			<li>
+				<img src="/sdysports/Public/images/set1.gif">
+				<div>
+					<p>
+						<span class="title">定向篮球</span>
+						<a href="#">即刻启程</a>
+						<span class="rank rank3"></span>
+					</p>
+				</div>
+			</li>			
+		</ul>
+		<ul id="showBox">
+			<li>
+				<a href="#"><img src="/sdysports/Public/images/show1.jpg"></a>
+			</li>
+			<li>
+				<a href="#"><img src="/sdysports/Public/images/show2.jpg"></a>
+			</li>
+			<li>
+				<a href="#"><img src="/sdysports/Public/images/show3.jpg"></a>
+			</li>
+			<li>
+				<a href="#"><img src="/sdysports/Public/images/show4.jpg"></a>
+			</li>			
+		</ul>
+	</div>
+	<div id="section3">
+		<div id="news_wrapper">
+			<ul>
+				<li>
+					<img src="/sdysports/Public/images/news1.jpg">
+				</li>
+				<li>
+					<img src="/sdysports/Public/images/news2.jpg">
+				</li>
+				<li class="shortcut">
+					<h1>西安活动顺利开展！</h1>
+					<h2>2015年7月</h2>
+					<p>经过紧张的比赛之后，西安赛区成功决出胜负经过紧张的比赛之后，西安赛区成功决出胜负经过紧张的比赛之后，西安赛区成功决出胜负经过紧张的比赛之后，西安赛区成功决出胜负</p>				
+				</li>
+			</ul>			
+			<ul>
+				<li class="shortcut">
+					<h1>西安活动顺利开展！</h1>
+					<h2>2015年7月</h2>
+					<p>经过紧张的比赛之后，西安赛区成功决出胜负经过紧张的比赛之后，西安赛区成功决出胜负经过紧张的比赛之后，西安赛区成功决出胜负经过紧张的比赛之后，西安赛区成功决出胜负</p>
+				</li>
+				<li>
+					<img src="/sdysports/Public/images/news5.jpg">
+				</li>
+				<li>
+					<img src="/sdysports/Public/images/news6.jpg">
+				</li>				
+			</ul>
+			<ul>
+				<li>
+					<img src="/sdysports/Public/images/news3.jpg">
+				</li>
+				<li class="shortcut">
+					<h1>西安活动顺利开展！</h1>
+					<h2 class="hot">2015年7月</h2>
+					<p>经过紧张的比赛之后，西安赛区成功决出胜负经过紧张的比赛之后，西安赛区成功决出胜负经过紧张的比赛之后，西安赛区成功决出胜负经过紧张的比赛之后，西安赛区成功决出胜负</p>
+				</li>
+				<li>
+					<img src="/sdysports/Public/images/news4.jpg">
+				</li>				
+			</ul>
+			<ul>
+				<li>
+					<img src="/sdysports/Public/images/news6.jpg">
+				</li>
+				<li>
+					<img src="/sdysports/Public/images/news7.jpg">
+				</li>
+				<li class="shortcut">
+					<h1>西安活动顺利开展！</h1>
+					<h2 class="hot">2015年7月</h2>
+					<p>经过紧张的比赛之后，西安赛区成功决出胜负经过紧张的比赛之后，西安赛区成功决出胜负经过紧张的比赛之后，西安赛区成功决出胜负经过紧张的比赛之后，西安赛区成功决出胜负</p>
+				</li>
+			</ul>
+			<div style="clear:both;"></div>
+		</div>		
+	</div>
+	<div id="cooperator">
+		<div id="intro">
+			<h1><img src="/sdysports/Public/images/logo_trans.gif"></h1>
+			<p>思迪亚团队坚持以青少年体育教育为中心，以奋斗者为本，基于青少年成长需求持续创新，自主开发适合青少年德智体美全方发展的培训课程，通过实践赢得了众多学生及家长的青睐。在国内外同行业交流会上，我们独立创新的培训课程在体育培训机构中得到肯定。</p>
+			<p>在青少年体育教育为核心基础上，我们放眼世界，2012年受到NBA中国的关注，帮助其在中国完成中国赛及篮球训练营等相关赛事及活动。与NBA中国良好合作背景下，我们还帮助Adidas、Nike、李宁、匹克等国内外知名体育产业公司完成赛事活动，都获得了非常高的评价 。</p>
+			<p>2015年思迪亚更是与西城区体育局育星俱乐部强强联手，不断突破，通过同北京体育大学，美国知名高校的不断交流研究，合力打造最适合中国青少年健康成长的篮球培训课程，真正做到体育，心理，文化共同发展的专属课程。</p>
+		</div>		
+		<div id="addr">
+			<img src="/sdysports/Public/images/map.gif">
+			<hgroup>
+				<h1>
+					思迪亚体育北京总部<br>
+					SDY Beijing Head Office
+				</h1>
+				<h2>
+					思迪亚（北京）体育文化发展有限公司<br>
+					SDY (Beijing) Sh2orts Culture Development Co., Ltd
+				</h2>
+				<h3>
+					A:北京市通州区新华西街万达广场B座2719室<br>
+					Room 2719,NO.58, Block B, Xinhua Street, Tongzhou District, Beijing Wanda Plaza
+				</h3>
+			</hgroup>
+		</div>
+		<p style="clear:both;"></p>
+	</div>
+	<footer>
+		<div id="sns">			
+			<a href="http://weibo.com/u/5642419086" target="_blank" class="sns_weibo"></a>
+			<a href="#" class="sns_weixin"></a>
+	  		<div id="weixin_code"></div>
+		</div>
+	</footer>
+</body>
+<script type="text/javascript" src="/sdysports/Public/js/jquery.min.js"></script>
+<script type="text/javascript" src="/sdysports/Public/js/jquery.fullScreenSlider.js"></script>
+<script type="text/javascript" src="/sdysports/Public/js/section2.js"></script>
+<script type="text/javascript" src="/sdysports/Public/js/share.js"></script>
+<script>
+	$(window).fullScreenSlider();
+</script>
+</html>
